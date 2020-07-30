a59299c4c1d985039fc631f1147568ea14043bfe
frances-hansen <francesjuniperhansen@users.noreply.github.com>
2020-07-29 23:41:25 -0700

    grid

--- code.js
+++ code.js
@@ -1,4 +1,4 @@
-document.body.onload = addElement
+//document.body.onload = addElement
 
 // global variables
 let rows = 24;
@@ -9,33 +9,36 @@ function initialize() {
 }
 // createe the board
 function createTable() {
-
-}
-
-// begin!
-window.onload = initialize
-function addElement () {
-    let newDiv = document.createElement("div")
-    let table = document.createElement("table")
-
+    let gridContainer = document.getElementById("gridContainer");
+    if (!gridContainer) {
+        console.error("ERROR: NO DIV FOR GRID TABLE.")
+    }
+    let table = document.createElement("table");
     // looping through the table to make rows
-
     // outer loop that iterates through every row
     // i represents the row we are currently on as we go through the table :)
-    for (let i = 0; i < rows; i++ {
+    for (let i = 0; i < rows; i++) {
         let tr = document.createElement("tr");
         // j iterates 24 times and creates a td element for each iteration
         for (var j = 0; j < cols; j++) {
             let cell = document.createElement("td");
             // each cell will need a unique id so we can pick it out later
             // id is created by using the row # on i and cols # j plus an underscore
-            cell.setAttribute("id", i + "_" + j)
+            cell.setAttribute("id", i + "_" + j);
             // initiate all cells to start with the dead class
-            cell.setAttribute("class", "dead")
+            cell.setAttribute("class", "dead");
+            tr.appendChild(cell);
         }
-    })
-    newDiv.appendChild(rows)
-    let currentDiv = document.getElementById("gridContainer");
-    document.body.insertBefore(newDiv, currentDiv)
+        table.appendChild(tr)
+    }
+    gridContainer.appendChild(table)
 }
+
+// begin!
+window.onload = initialize
+
+
+
+
+
 
